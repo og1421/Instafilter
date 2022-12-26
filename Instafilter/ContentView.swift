c254b3c0773a70f5b6e0a960985d0036292e035e
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-26 17:03:28 -0300

    learning confirmationDialog

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -7,13 +7,26 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View{
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .alert("Change background", isPresented: $showingConfirmation) {
+                    Button("Red"){ backgroundColor = .red }
+                    Button("Green") { backgroundColor = .green }
+                    Button("Blue") { backgroundColor = .blue }
+                    Button("Cancel", role: .cancel){ }
+                } message: {
+                    Text("Select a new color")
+                }
         }
         .padding()
     }
