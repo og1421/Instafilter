49512cf31c61e5b22c3c3daf37b411d7be58510e
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-26 16:27:49 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Orlando Moraes Martins on 26/12/22.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
