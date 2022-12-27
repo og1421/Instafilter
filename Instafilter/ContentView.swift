b5b0d59ea35eacf14b16acaec1ff0f1f5345bfd7
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-27 11:25:22 -0300

    ImagePicker ended

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 struct ContentView: View{
     @State private var image: Image?
+    @State private var inputImage: UIImage?
     @State private var showingImagePicker = false
     
     var body: some View {
@@ -24,8 +25,14 @@ struct ContentView: View{
             }
         }
         .sheet(isPresented: $showingImagePicker){
-            imagePicker()
+            imagePicker(image: $inputImage)
         }
+        .onChange(of: inputImage){ _ in loadImage()   }
+    }
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        
+        image = Image(uiImage: inputImage)
     }
 }
 
